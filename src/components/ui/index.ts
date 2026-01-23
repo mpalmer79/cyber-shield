ddb8e1f5a222e8c623de7759b22df7cd0822fdaf
mpalmer79 <mpalm2025@gmail.com>
2026-01-23 16:24:36 -0500

    Create index.ts

---
+++ src/components/ui/index.ts
@@ -0,0 +1,17 @@
+// UI Components Index
+export { default as Skeleton, ModuleGridSkeleton, LeaderboardSkeleton, ProgressStatsSkeleton, ChatSkeleton, DashboardStatsSkeleton } from './Skeleton';
+export { default as Toast, ToastContainer, useToast } from './Toast';
+export { default as Onboarding, useOnboarding } from './Onboarding';
+export { default as EmptyState, NoSearchResults, NoProgressData, NoBadgesEarned, ConnectionError } from './EmptyState';
+export { default as ThemeToggle, ThemeProvider, useTheme } from './ThemeToggle';
+export { 
+  playSound, 
+  useSoundEffect, 
+  useHapticFeedback,
+  InteractiveButton,
+  AnimatedCounter,
+  AnimatedProgress,
+  PulseWrapper,
+  ShakeWrapper,
+  Confetti
+} from './MicroInteractions';
