5b74be06a9e204c2214405309e8da1647fac3c39
mpalmer79 <mpalm2025@gmail.com>
2026-01-23 16:28:56 -0500

    Create index.ts

---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+export { default as Header } from './Header';
+export { default as ModuleCard } from './ModuleCard';
+export { default as FeatureCard, HeroFeatureCard, StatsFeatureCard } from './FeatureCard';
+export { default as PhishingScenario } from './PhishingScenario';
+export { default as ChatInterface } from './ChatInterface';
+export { default as ScoreDisplay } from './ScoreDisplay';
+export { default as ProgressStats } from './ProgressStats';
+
+// UI Components
+export * from './ui';
