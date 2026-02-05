2760ae011bb567a2a2000d0ab16b4de0a5d43eeb
mpalmer79 <mpalm2025@gmail.com>
2026-02-05 12:45:25 -0500

    Create index.ts

---
+++ src/components/simulators/index.ts
@@ -0,0 +1,4 @@
+// src/components/simulators/index.ts
+export { default as EmailSimulator } from './EmailSimulator';
+export { default as SMSSimulator } from './SMSSimulator';
+export { default as PhoneCallSimulator } from './PhoneCallSimulator';
