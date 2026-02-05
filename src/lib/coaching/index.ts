03989be9475c8ddbdb4a339d6c6ba39eb4b0d45b
mpalmer79 <mpalm2025@gmail.com>
2026-02-05 12:18:01 -0500

    Create index.ts

---
+++ src/lib/coaching/index.ts
@@ -0,0 +1,12 @@
+// src/lib/coaching/index.ts
+export {
+  CoachingEngine,
+  getGradeColor,
+  getGradeBg,
+} from './engine';
+
+export type {
+  CoachMessage,
+  CoachingSession,
+  CoachingScoreBreakdown,
+} from './engine';
